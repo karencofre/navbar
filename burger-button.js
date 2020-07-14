aa5b11a228353c0c5b920eff91412c223a2b8e5c
karencofre <karencofrecejas@gmail.com>
2020-07-13 22:15:37 -0400

    nav terminada

---
+++ burger-button.js
@@ -0,0 +1,10 @@
+console.log("hola");
+btn = document.querySelector("#burger-button");
+menu = document.querySelector(".menu");
+btn.addEventListener("click", function () {
+  if (menu.classList.contains("is-active")) {
+    menu.classList.remove("is-active");
+  } else {
+    menu.classList.add("is-active");
+  }
+});
